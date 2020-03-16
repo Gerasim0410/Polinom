94df40aee6d6e35b6faa29fbe48f3b421752048c
Тюрина Дарья <tyurina0410@gmail.com>
2020-03-16 14:33:11 +0300

    Update Source.cpp

--- CppCLR_WinformsProjekt4/List/Source.cpp
+++ CppCLR_WinformsProjekt4/List/Source.cpp
@@ -8,12 +8,25 @@
 
 int main() {
 
-	int a[2][2];
+	/*int a[2][2];
 	for (int i = 0; i < 2; i++) {
 		cin >> a[i][0];
 		cin >> a[i][1];
+	}*/
+
+	TMonom g;
+	g.coeff = 5;
+	g.px = 1;
+	g.py = 1;
+	g.pz = 1;
+	TPolinom p;
+	while (g.coeff) {
+		cin >> g.coeff;
+		cin >> g.px;
+		cin >> g.py;
+		cin >> g.pz;
+		p.insOrd(g);
 	}
-	TPolinom p(a, 2);
 	TPolinom res, q(p);
 	TMonom m;
 	m.coeff = 5;
@@ -22,7 +35,11 @@ int main() {
 	m.pz = 1;
 	p *= m;
 	p.insOrd(m);
+
+	cout << res << endl;
+	cout << q << endl;
 	cout << p << endl;
+	cout << m << endl;
 	p += p;
 	cout << p << endl;
 	res = p*q;
@@ -33,6 +50,9 @@ int main() {
 	cout << res << endl;
 	res *= 2;
 	cout << res << endl;
+
+
+
 	_getch();
 
 	return 0;
